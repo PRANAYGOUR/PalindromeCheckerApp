49e72e1b240b22ec709ab8a500be0be9092cd713
PRANAYGOUR <pranaygour3042007@gmail.com>
2026-02-23 09:29:21 +0530

    UC2:Hardcoded palindrome

--- src/UseCase1PalindromeApp.java
+++ src/UseCase1PalindromeApp.java
@@ -1,8 +1,37 @@
+import java.util.Scanner;
 public class UseCase1PalindromeApp {
+
     public static void main(String args[]){
-        System.out.println("Welcome to the Palindrome Checker Management System");
+        /*System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version : 1.0");
-        System.out.println("System Initialized Successfully");
+        System.out.println("System Initialized Successfully");*/
+        Scanner scanner = new Scanner(System.in); //
+        System.out.println("Enter a sentence:");
+        String inputString = scanner.nextLine();
+        //System.out.println("Entered String: "+inputString);
+
+        if(inputString == null || inputString.trim().isEmpty()){
+            System.out.println("Empty String, Please enter a String");
+            return;
+        }
+
+
+        boolean Isplaindrome = true;
+        for(int i=0;i<inputString.length()/2;i++){
+            if(inputString.charAt(i) != inputString.charAt((inputString.length())-i-1)) {
+                Isplaindrome = false;
+            }
+        }
+        if(Isplaindrome){
+            System.out.println("Is it a palindrome: " + Isplaindrome);
+        }
+        else{
+            System.out.println("Is it a palindrome: " + Isplaindrome);
+
+        }
+
+
+
 
     }
 }
